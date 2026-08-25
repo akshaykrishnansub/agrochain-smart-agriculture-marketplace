c1a17c86e6380ff53db9f82d7e51f147d4c9b0f5
unknown <akshaykrish533516@gmail.com>
2026-08-25 14:48:51 +0530

    Login API implemented

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,6 +1,8 @@
 import type { Request,Response } from "express"
 import bcrypt from 'bcrypt'
 import { createUser, findUserByEmail } from "../services/userService.js";
+import User from "../models/User.js";
+import jwt from 'jsonwebtoken'
 
 export const register=async(req:Request,res:Response)=>{
     try{
@@ -19,6 +21,39 @@ export const register=async(req:Request,res:Response)=>{
         const user=await createUser({name,email,passwordHash,role:"buyer"});
 
         return res.status(201).json({message:'User Registered Successfully',user:{id:user._id,name:user.name,email:user.email,role:user.role}});
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({error:'Internal Server Error'});
+    }
+}
+
+export const login=async(req:Request,res:Response)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({message:'Email and Password are required'});
+        }
+
+        const user=await User.findOne({email});
+
+        if(!user){
+            return res.status(401).json({message:'Invalid email or password'});
+        }
+
+        const isValidPassword=await bcrypt.compare(password,user.passwordHash);
+
+        if(!isValidPassword){
+            return res.status(401).json({message:'Invalid email or password'});
+        }
+
+        const token=jwt.sign({userId:user._id,role:user.role},process.env.JWT_SECRET!,{expiresIn:'1d'});
+
+        return res.status(200).json({message:'Login Successful',accessToken:token,user:{
+            id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role
+        }})
     }catch(err){
         console.error(err);
         return res.status(500).json({error:'Internal Server Error'});
